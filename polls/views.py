7cf1c7947fea13c9aef2928568e41f6ba0c4ea80
hayasnc <hayasnc@gmail.com>
2019-07-26 15:45:29 +0530

    Added Models to Admin site, Routing added

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def details(request, question_id):
+    return HttpResponse("You're viewing Question with id %s" %question_id)
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response %question_id)
+
+def voting(request, question_id):
+    return HttpResponse("Voting for Question %s" %question_id)
